cb63d7a7e58af8f63da10d12f6a2fb159747df39
AntonDrazdou <woodland.by@gmail.com>
2019-10-15 22:58:49 +0300

    feat: add solution

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,19 +1,65 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+    constructor() {
+        this._head = new Node();
+        this._tail = new Node();
+        this.length = 0;
+    }
 
-    append(data) {}
+    append(data) {
+            if (this.length === 0) {
+                this._head = new Node(data);
+                this._tail = new Node(data);
+                this.length++;
+            } else {
+                this._tail = this._head;
+                while(this._tail.next !==null) {
+                    this._tail = this._tail.next;
+                }
+                this._tail.next = new Node(data);
+                this._tail.next.prev = this._tail
+                this._tail = this._tail.next;
+                this.length++;
+                
+            }
+            
+        }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        let currentNode = this._head;
+        while (index != 0) {
+            currentNode = currentNode.next;
+            index--;
+        }
+        return currentNode.data;
+    }
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        let currentNode = this._head;
+        while (index != 0) {
+            currentNode = currentNode.next;
+            index--;
+        }
+        let next = currentNode.next;
+        let prev = currentNode.prev;
 
-    isEmpty() {}
+        let newNode = new Node(data, prev, currentNode);
+        currentNode.prev = newNode;
+        prev.next = newNode;
+    }
+
+    isEmpty() {
+        
+    }
 
     clear() {}
 
